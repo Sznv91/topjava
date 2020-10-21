549b1a58b9d91616b07d87cd01c696ca63268ff1
Sznv91 <47347643+Sznv91@users.noreply.github.com>
2020-10-21 17:55:42 +0300

    Make init table Meals in DB

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -9,3 +9,7 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, description, datetime, calories)
+VALUES (100000, 'Админ ланч','2015-06-01 14:00:00',510),
+       (100000, 'Админ ужин','2015-06-01 21:00:00',1500);
