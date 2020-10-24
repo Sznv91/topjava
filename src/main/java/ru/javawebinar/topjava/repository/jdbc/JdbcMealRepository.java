ec6313f292735e601de3fcd5c2a6ad4801e4631d
Sznv91 <47347643+Sznv91@users.noreply.github.com>
2020-10-24 19:44:23 +0300

    make JdbcMealRepository and part of Tests

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
@@ -1,37 +1,77 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import java.math.BigInteger;
+import java.sql.ResultSet;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Repository
 public class JdbcMealRepository implements MealRepository {
+//https://alexkosarev.name/2016/06/13/spring-framework-jdbctemplate/
+
+    RowMapper<Meal> ROW_MAPPER = (ResultSet resultSet, int rowNum) -> {
+        int id = resultSet.getInt("id");
+        LocalDateTime dateTime = LocalDateTime.parse(resultSet.getString("datetime").substring(0, 19), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        int calories = resultSet.getInt("calories");
+        int userId = resultSet.getInt("user_id");
+        String description = resultSet.getString("description");
+        return new Meal(id, dateTime, description, calories);
+    };
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcMealRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        jdbcTemplate.update("INSERT INTO meals(user_id, description, datetime, calories) " +
+                "VALUES (?,?,?,?)", userId, meal.getDescription(), meal.getDateTime(), meal.getCalories());
+        int seq = Integer.parseInt(
+                jdbcTemplate.query("SELECT * FROM global_seq", (rse, rowNum) -> rse.getString("last_value")).get(0));
+        return get(seq, userId);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        int result = jdbcTemplate.update("DELETE FROM meals WHERE id =? AND user_id = ?", id, userId);
+        return result > 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        Meal result = null;
+        try {
+            result = jdbcTemplate.queryForObject("SElECT * FROM meals WHERE id=?", new Object[]{id}, ROW_MAPPER);
+        } catch (EmptyResultDataAccessException e) {
+            //LOG not found
+            return null;
+        }
+        int userIdFromDb = jdbcTemplate.query("SELECT user_id FROM meals WHERE id=?"
+                , new Object[]{id}
+                , (ResultSet rs, int rowInt) -> rs.getInt("user_id")).get(0);
+        return userId == userIdFromDb ? result : null;
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return jdbcTemplate.query("SELECT * FROM meals WHERE user_id = ?", new Object[]{userId}, ROW_MAPPER);
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
+        List<Meal> result = jdbcTemplate.query("SELECT * FROM meals WHERE user_id=? AND datetime >= ? AND datetime < ?", new Object[]{userId, startDateTime, endDateTime}, ROW_MAPPER);
         return null;
     }
 }
---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,73 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"})
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+
+public class MealServiceTest {
+
+    @Autowired
+    private MealService service;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void get() {
+        Meal actual = service.get(100002, 100000);
+        Meal except = new Meal(100002, LocalDateTime.of(2015, 06, 01, 14, 00), "Админ ланч", 510);
+
+        assertEquals(except.getId(), actual.getId());
+        assertEquals(except.getDateTime().toString(), actual.getDateTime().toString());
+        assertEquals(except.getDescription(), actual.getDescription());
+        assertEquals(except.getCalories(), actual.getCalories());
+    }
+
+    @Test
+    public void delete() {
+        service.delete(100002, 100000);
+        List<Meal> list = service.getAll(100000);
+        assertEquals(list.size(), 1);
+//        assertThrows(EmptyResultDataAccessException.class, () -> service.get(100002, 100000));
+        assertThrows(NotFoundException.class, () -> service.get(100002, 100000));
+
+    }
+
+    @Test
+    public void getBetweenInclusive() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void create() {
+        Meal toWrite = new Meal(LocalDateTime.now(), "Еда Junit", 1488);
+        System.out.println(toWrite.getId() + "Expect NOT NULL");
+        service.create(toWrite, 100000);
+    }
+}
